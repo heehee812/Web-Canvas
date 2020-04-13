315c9a45edfd1036f114749c1eb88621de7a9076
恩宇 <e.enyuu812@gmail.com>
2020-04-14 01:07:08 +0800

    add function DOWNLOAD, UPLOAD, TEXT(error)

--- canvas.js
+++ canvas.js
@@ -1,10 +1,13 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
-var x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+var x1 = 0,
+    y1 = 0,
+    x2 = 0,
+    y2 = 0;
 var modee = "pencil";
 var hasTouchEvent = 'ontouchstart' in window ? true : false;
 let state = ctx.getImageData(0, 0, canvas.width, canvas.height);
-window.history.pushState(state, null);//put state in here
+window.history.pushState(state, null); //put state in here
 window.addEventListener('popstate', lastStep, false);
 window.addEventListener('load', mode);
 
@@ -21,7 +24,7 @@ function changeMode(a) {
 }
 
 function mode() {
-    canvas.addEventListener(downEvent, function (e) {
+    canvas.addEventListener(downEvent, function(e) {
         console.log(modee);
         if (modee == "text") {
             x1 = e.offsetX;
@@ -31,12 +34,11 @@ function mode() {
             ctx = canvas.getContext('2d');
             ctx.globalCompositeOperation = "source-over";
             isMouseActive = true;
-            document.getElementById("text-input").addEventListener('keyup', function (e) {
+            document.getElementById("text-input").addEventListener('keyup', function(e) {
                 console.log(e.keyCode);
                 if (e.keyCode == 13) {
                     y1 += 36;
-                }
-                else {
+                } else {
                     ctx.fillStyle = "black";
                     ctx.textBaseline = "middle";
                     ctx.font = "36px 'Monserrat";
@@ -46,8 +48,7 @@ function mode() {
                 }
             })
 
-        }
-        else if (modee == "pencil") {
+        } else if (modee == "pencil") {
             ctx = canvas.getContext('2d');
             ctx.globalCompositeOperation = "source-over";
             isMouseActive = true;
@@ -56,24 +57,19 @@ function mode() {
             ctx.lineJoin = 'round';
             x1 = e.offsetX;
             y1 = e.offsetY;
-        }
-        else if (modee == "circle") {
+        } else if (modee == "circle") {
             ctx = canvas.getContext('2d');
             ctx.globalCompositeOperation = "source-over";
             isMouseActive = true;
             x1 = e.offsetX;
             y1 = e.offsetY;
-        }
-        else if (modee == "square") {
+        } else if (modee == "square") {
             ctx = canvas.getContext('2d');
             ctx.globalCompositeOperation = "source-over";
             isMouseActive = true;
             x1 = e.offsetX;
             y1 = e.offsetY;
-        }
-
-
-        else if (modee == "eraser") {
+        } else if (modee == "eraser") {
             ctx = canvas.getContext('2d');
             ctx.globalCompositeOperation = "destination-out";
             isMouseActive = true;
@@ -82,7 +78,7 @@ function mode() {
         }
     })
 
-    canvas.addEventListener(moveEvent, function (e) {
+    canvas.addEventListener(moveEvent, function(e) {
         if (!isMouseActive) {
             return;
         }
@@ -95,23 +91,20 @@ function mode() {
             ctx.stroke();
             x1 = x2;
             y1 = y2;
-        }
-        else if (modee == "circle") {
+        } else if (modee == "circle") {
             x2 = e.offsetX;
             y2 = e.offsetY;
             ctx.fillStyle = "#621708";
             ctx.beginPath();
             ctx.arc(x1, y1, Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)), 0, Math.PI * 2);
             ctx.fill();
-        }
-        else if (modee == "square") {
+        } else if (modee == "square") {
             x2 = e.offsetX;
             y2 = e.offsetY;
             ctx.fillStyle = "#941B0C";
             ctx.moveTo(x1, y1);
             ctx.lineTo(x2, y2);
-        }
-        else if (modee == "eraser") {
+        } else if (modee == "eraser") {
             x2 = e.offsetX;
             y2 = e.offsetY;
             ctx.beginPath();
@@ -122,13 +115,12 @@ function mode() {
         }
     })
 
-    canvas.addEventListener(upEvent, function (e) {
+    canvas.addEventListener(upEvent, function(e) {
         isMouseActive = false;
-        if(modee =="square")
-        {
+        if (modee == "square") {
             ctx.fillRect(x1, y1, x2, y2);
         }
-        state= ctx.getImageData(0, 0, canvas.width, canvas.height);
+        state = ctx.getImageData(0, 0, canvas.width, canvas.height);
         window.history.pushState(state, null);
 
     })
@@ -162,7 +154,7 @@ function redo() {
     window.history.go(1);
 }
 
-function lastStep(e)//clear the current canvas and get the last staep.
+function lastStep(e) //clear the current canvas and get the last staep.
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     if (e.state) {
@@ -170,3 +162,25 @@ function lastStep(e)//clear the current canvas and get the last staep.
     }
 }
 
+function download() {
+    var link = document.createElement('a');
+    link.download = 'myCanvasImage.png';
+    link.href = canvas.toDataURL();
+    link.click();
+}
+
+upload.onchange = function upload() {
+    var img = new Image();
+
+    img.onload = function() {
+        canvas.width = this.width
+        canvas.height = this.height
+        ctx.drawImage(this, 0, 0, canvas.width, canvas.height)
+        URL.revokeObjectURL(src)
+    }
+
+    var file = this.files[0];
+    var src = URL.createObjectURL(file);
+
+    img.src = src;
+}
