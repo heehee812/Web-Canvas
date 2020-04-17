449d3e8f81e7717d752480beceb87fa9961bf897
EnYu <57358478+heehee812@users.noreply.github.com>
2020-04-17 18:51:53 +0800

    Delete function resizeCanvas().js

--- function resizeCanvas().js
+++
@@ -1,7 +0,0 @@
-function resizeCanvas()
-{
-    var canvas= document.getElementById('_2DCanvas');
-    canvas.setAttribute('width', window.innerWidth);
-    canvas.setAttribute('height', window.innerHeight);
-}
-window.onresize= resizeCanvas;
